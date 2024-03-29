96dcb0d9c382db6b66e9f689c0841bc68eafab16
WurstCommander <wursch@gmx.de>
2024-03-29 02:40:03 +0100

    Update titanfall2_multifix.ps1

--- titanfall2_multifix.ps1
+++ titanfall2_multifix.ps1
@@ -1,11 +1,12 @@
 # Configuration - Path to Titanfall2.exe
-$pathTF2 = "D:\Spiele\Titanfall2\Titanfall2.exe"
+# Change to your install location:
+$pathTF2 = "D:\MyGameFolder\Titanfall2\Titanfall2.exe"
 
+# Actual Code
 # Check for administrative privileges
 function CheckAdmin {
     $currentUser = [Security.Principal.WindowsIdentity]::GetCurrent()
-    $isAdmin = (New-Object Security.Principal.WindowsPrincipal $currentUser).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
-    
+    $isAdmin = (New-Object Security.Principal.WindowsPrincipal $currentUser).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)    
     if (!$isAdmin) {
         Write-Host "---------------------------------------------------------------------"
         Write-Host "Administrator rights not found. Please start as an administrator!"
@@ -51,17 +52,13 @@ Write-Host "--------------------------------------------------------------------
 Write-Host "Setting OPENSSL_ia32cap environment variable to help Titanfall 2 find multiplayer servers..."
 Write-Host "------------------------------------------------------------------------------------------------"
 [Environment]::SetEnvironmentVariable("OPENSSL_ia32cap", "~0x200000200000000", "Machine")
-
 # Start Titanfall 2
 Write-Host "------------------------------------------------------------------------------------------------"
 Write-Host "Environment variable set, launching Titanfall 2..."
 Write-Host "------------------------------------------------------------------------------------------------"
-
 Start-Process -FilePath $pathTF2
 WaitForTitanfall2ToStart
-
 # Clean up environment variable after game launch
 Write-Host "Cleaning up the environment variable..."
 [Environment]::SetEnvironmentVariable("OPENSSL_ia32cap", "", "Machine")
-
 exit
